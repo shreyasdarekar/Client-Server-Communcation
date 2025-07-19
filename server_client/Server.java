3b0564028d204a0355886a42bb97af8efdcfbd2d
Vaibhav Anil Vyavahare <vaibhavvyavahare20@gmail.com>
2025-07-19 15:45:28 +0530

    deleting old code

--- server_client/Server.java
+++ server_client/Server.java
@@ -23,57 +23,7 @@ public class Server {
 
     }
 
-    public void Server101() {
-        try {
-            ServerSocket var1 = new ServerSocket(this.port);
 
-            try {
-                System.out.println("Server is listening on port " + this.port);
-
-                while(true) {
-                    while(true) {
-                        try {
-                            Socket var2 = var1.accept();
-                            PrintStream var10000 = System.out;
-                            String var10001 = String.valueOf(var2.getInetAddress());
-                            var10000.println("Client connected from " + var10001 + ":" + var2.getPort());
-                            String var3 = "Welcome to the server!";
-                            var2.getOutputStream().write(var3.getBytes());
-                            var2.getOutputStream().flush();
-
-                            try {
-                                BufferedReader var4 = new BufferedReader(new InputStreamReader(var2.getInputStream()));
-                                String var5 = var4.readLine();
-                                System.out.println(var5);
-                            } catch (IOException var7) {
-                                System.err.println(":" + String.valueOf(var7));
-                            }
-
-                            var2.close();
-                            var10000 = System.out;
-                            var10001 = String.valueOf(var2.getInetAddress());
-                            var10000.println("Closed connection with " + var10001 + ":" + var2.getPort());
-                        } catch (IOException var8) {
-                            System.err.println("Error handling client connection: " + var8.getMessage());
-                            var8.printStackTrace();
-                        }
-                    }
-                }
-            } catch (Throwable var9) {
-                try {
-                    var1.close();
-                } catch (Throwable var6) {
-                    var9.addSuppressed(var6);
-                }
-
-                throw var9;
-            }
-        } catch (IOException var10) {
-            System.err.println("Error creating server socket: " + var10.getMessage());
-            var10.printStackTrace();
-            System.out.println("server_client.Server Exiting");
-        }
-    }
 
 
     public void startServer(){
